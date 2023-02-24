bc2a89ca631f17007b658d5a319579deef966940
itwill <itwill@BOOK-0M42QBIBM5>
2023-02-24 09:50:09 +0900

    ..

--- spring_application_dao/src/test/java/com/itwill/user/dao/jdbctemplate/UserDaoImplJdbcTemplateTest.java
+++ spring_application_dao/src/test/java/com/itwill/user/dao/jdbctemplate/UserDaoImplJdbcTemplateTest.java
@@ -3,29 +3,39 @@ package com.itwill.user.dao.jdbctemplate;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.function.Supplier;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.transaction.annotation.Transactional;
+
+
 @SpringBootApplication
 @SpringBootTest
 class UserDaoImplJdbcTemplateTest {
+	Logger logger=LoggerFactory.getLogger(UserDaoImplJdbcTemplate.class);
+	
 	@Autowired
 	UserDao userDao;
-	
+	@Transactional
 	@Test
 	void testCreate() {
 		User user=new User("jdbctemplate34", "1111", "제이디비씨", "jdbc@jdbc.com");
 		try {
 			int rowCount = userDao.create(user);
-			assertEquals(rowCount, 1);
+			assertEquals(rowCount, 1,"insert success");
+			
 		} catch (Exception e) {
 			//fail(e.getMessage());
 			assertInstanceOf(DuplicateKeyException.class,e);
 		}
+		
 	}
 	
 	@Disabled
