9725ee0b240c23bdfcb3b11bc06c2abdb397b5c0
itwill <itwill@BOOK-0M42QBIBM5>
2023-02-28 15:22:35 +0900

    ..

--- spring_web_application/src/main/java/com/itwill/controller/HelloAnnotationMultiRequestMappingController.java
+++ spring_web_application/src/main/java/com/itwill/controller/HelloAnnotationMultiRequestMappingController.java
@@ -17,7 +17,28 @@ public class HelloAnnotationMultiRequestMappingController {
 	public String hello5() {
 		return "forward:/WEB-INF/views/hello5.jsp";
 	}
-		
+	
+	@RequestMapping("/hello_redirect_servlet")
+	public String hello_redirect_servlet() {
+		return "redirect:hello_redirected_servlet";
+	}
+	@RequestMapping("/hello_redirected_servlet")
+	public String hello_redirected_servlet() {
+		return "forward:/WEB-INF/views/hello_redirected_servlet.jsp";
+	}
+	
+	@RequestMapping("/hello_servlet_forward")
+	public String hello_servlet_forward() {
+		return "forward:hello_servlet_forwarded";
+	}
+	
+	@RequestMapping("/hello_servlet_forwarded")
+	public String hello_servlet_forwarded() {
+		return "forward:/WEB-INF/views/hello_servlet_forwarded.jsp";
+	}
+	
+	
+	
 }
 
 
