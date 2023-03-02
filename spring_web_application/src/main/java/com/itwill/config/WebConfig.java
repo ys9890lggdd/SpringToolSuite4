404cb763bdf3e2b867ac6cfb616925ddfc6f5c8c
itwill <itwill@BOOK-0M42QBIBM5>
2023-03-02 12:04:10 +0900

    ..

--- spring_web_application/src/main/java/com/itwill/config/WebConfig.java
+++ spring_web_application/src/main/java/com/itwill/config/WebConfig.java
@@ -4,19 +4,38 @@ import java.util.Properties;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.itwill.controller.HelloController;
 
 @Configuration
+//@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer{
 	
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("forward:/index.jsp");
 	}
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		 //registry.jsp("/WEB-INF/views",".jsp");
+	}
+	/************************spring mvc객체등록*****************************/
+	@Bean
+	public BeanNameViewResolver beanNameViewResolver() {
+		BeanNameViewResolver beanNameViewResolver=new BeanNameViewResolver();
+		beanNameViewResolver.setOrder(0);
+		return beanNameViewResolver;
+	}
+	
+	
+	
 	/*
 	@Bean
 	public SimpleUrlHandlerMapping sampleServletMapping() {
