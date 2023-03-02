b13c73e672c12c7f4f091c883d6ee8be6ac7ae35
itwill <itwill@BOOK-0M42QBIBM5>
2023-03-02 13:45:19 +0900

    ..

--- spring_web_application/src/main/java/com/itwill/config/WebConfig.java
+++ spring_web_application/src/main/java/com/itwill/config/WebConfig.java
@@ -44,6 +44,7 @@ public class WebConfig implements WebMvcConfigurer{
 	}
 	
 	
+	
 	/*
 	@Bean
 	public SimpleUrlHandlerMapping sampleServletMapping() {
--- spring_web_application/src/main/java/com/itwill/controller/ResponseController.java
+++ spring_web_application/src/main/java/com/itwill/controller/ResponseController.java
@@ -12,6 +12,7 @@ import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.AbstractView;
 import org.springframework.web.servlet.view.BeanNameViewResolver;
 import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.itwill.view.XMLView;
@@ -58,22 +59,26 @@ public class ResponseController {
 		  4 . DispatcherServlet객체는 반환받은 InternalResourceView 객체의 renderMergedOutputModel()  메쏘드호출한다: 	
 			  (/WEB-INF/views/response_view_name.jsp 로 forward가이루어진다)	
 			  
-		  << mcv-config-view-resolver.xml >>
-		  <!-- InternalResourceViewResolver -->
-		 <bean 	id="myInternalResourceViewResolver" 
-			class="org.springframework.web.servlet.view.InternalResourceViewResolver">
-			<property name="order" value="1"/>
-			<property name="prefix" value="/WEB-INF/views/"/>
-			<property name="suffix" value=".jsp"/>
-		</bean>		
+		   /***********WebConfig.java***********
+		 	1. WebConfig.java파일에 InternalResourceViewResolver등록 
+		 	
+		 	@Bean
+			public InternalResourceViewResolver internalResourceViewResolver() {
+				InternalResourceViewResolver internalResourceViewResolver=new InternalResourceViewResolver();
+				internalResourceViewResolver.setPrefix("/WEB-INF/views/");
+				internalResourceViewResolver.setSuffix(".jsp");
+				internalResourceViewResolver.setOrder(1);
+				return internalResourceViewResolver;
+			}	
 		
-		< Controller반환 String >>
-			1.forward:/WEB-INF/views/response_view_name.jsp ==> prefix,suffix설정적용안됨
-		    2.response_forward_view_name   					==> prefix,suffix설정적용됨
-		    3.redirect:xxx.jsp     							==> prefix,suffix설정적용안됨
+		
+			< Controller반환 String >>
+				1.forward:/WEB-INF/views/response_view_name.jsp  ==> prefix,suffix설정적용안됨
+			    2.response_forward_view_name   					==> prefix,suffix설정적용됨
+			    3.redirect:xxx.jsp     							==> prefix,suffix설정적용안됨
 		 */
-		return "forward:/WEB-INF/views/response_forward_view_name.jsp";
-		//return "response_forward_view_name";
+		//return "forward:/WEB-INF/views/response_forward_view_name.jsp";
+		return "response_forward_view_name";
 	} 
 	
 	/*##################redirect###########################*/
@@ -182,17 +187,24 @@ public class ResponseController {
 			/*******ViewResolver객체등록**********************
 			1. WebConfig.java에 BeanNameViewResolver를 등록한다.
 			
-			@Bean
-			public BeanNameViewResolver beanNameViewResolver() {}
+				@Bean
+				public BeanNameViewResolver beanNameViewResolver() {
+					BeanNameViewResolver beanNameViewResolver=new BeanNameViewResolver();
+					beanNameViewResolver.setOrder(0);
+					return beanNameViewResolver;
+				}
 			
 			2.InternalResourceViewResolver 등록[빈으로 정의하지않아도 기본생성됨]
-			
-			<bean class="org.springframework.web.servlet.view.InternalResourceViewResolver">
-				<property name="order" value="1"/>
-				<!-- InternalResouceView객체생성후 DispatcherServlet에 반환시 forward path에 prefix,suffix -->
-				<property name="prefix" value="/WEB-INF/views/"/>
-				<property name="suffix" value=".jsp"></property>
-			</bean>
+			  설정을변경할려면 WebConfig.java파일에 InternalResourceViewResolver등록 
+		 	
+			 	@Bean
+				public InternalResourceViewResolver internalResourceViewResolver() {
+					InternalResourceViewResolver internalResourceViewResolver=new InternalResourceViewResolver();
+					internalResourceViewResolver.setPrefix("/WEB-INF/views/");
+					internalResourceViewResolver.setSuffix(".jsp");
+					internalResourceViewResolver.setOrder(1);
+					return internalResourceViewResolver;
+				}	
 			  
 		*/
 		
