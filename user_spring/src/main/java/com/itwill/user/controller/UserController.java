83be585ab1dfea10ce0462ab74070ca5b4e30624
itwill <itwill@BOOK-0M42QBIBM5>
2023-03-03 15:18:33 +0900

    ..

--- user_spring/src/main/java/com/itwill/user/controller/UserController.java
+++ user_spring/src/main/java/com/itwill/user/controller/UserController.java
@@ -1,17 +1,13 @@
 package com.itwill.user.controller;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.itwill.user.User;
 import com.itwill.user.UserService;
 /*
  * /user_main 
@@ -33,13 +29,19 @@ public class UserController {
 	public String user_main() {
 		return "user_main";
 	}
+	@RequestMapping("/user_write_form")
 	public String user_write_form() {
-		String forward_path = "";
+		String forward_path = "user_write_form";
 		return forward_path;
 	}
-
+    @PostMapping("user_write_action")
 	public String user_write_action_post() throws Exception {
 		String forward_path = "";
+		
+		
+		
+		
+		
 		return forward_path;
 	}
 
@@ -86,8 +88,14 @@ public class UserController {
 		String forwardPath = "";
 		return forwardPath;
 	}
+	
+	/***********GET방식요청시guest_main redurection**********/
+	@GetMapping({
+		        "user_write_action",
+		
+	            })
 
-	public String user_action_get() {
+	public String user_get() {
 		String forwardPath = "";
 		return forwardPath;
 	}
