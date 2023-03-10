9742e53dc78b52022987acabb35ef9d99fe96faf
itwill <itwill@BOOK-0M42QBIBM5>
2023-03-10 15:21:45 +0900

    ..

--- guest_javascript_ajax/WebContent/js/service.js
+++ guest_javascript_ajax/WebContent/js/service.js
@@ -2,11 +2,18 @@
  *
  */
 import {ajaxRequest} from './request.js';
-function guestService(){
-	let returnHJsonResult={};
-    ajaxRequest();
+function guestService(method,url,params){
 	//ajax요청
-
+	let returnHJsonResult={};
+    ajaxRequest(method,
+                url,
+                function(jsonResult){
+	                console.log(jsonResult);
+                },
+                params,
+                true);
+                
+	
 	returnJsonResult;
 }
 export{ guestService}
